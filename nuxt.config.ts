4dc92c1deb7015480f5235102be52feb576f146a
Sanath Kandikanti <sanathkandikanti@gmail.com>
2023-04-09 00:24:47 +0530

    Configured custom fonts and enabled treeshaking for vuetify
    * Configured custom google fonts
    * Moved sass to devDependencies
    * Enabled treeshaking for vuetify components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,31 @@
+import vuetify from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/google-fonts',
+        async (options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
+              vuetify({
+                  styles: {
+                    configFile: 'assets/settings.scss'
+                  }
+              })
+            ))
+          }
+    ],
     css: [
-        'vuetify/lib/styles/main.sass',
+      'vuetify/styles',
         '@mdi/font/css/materialdesignicons.min.css'
     ],
     build: {
         transpile: ['vuetify'],
+      },
+      typescript: {shim: false},
+      googleFonts: {
+        families: {
+          Caveat: [400, 500, 600, 700, 800]
+        },
+        download: false,
+        useStylesheet: true
       }
 })
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,27 @@
 <template>
     <section>
+
         <p>This page will be displayed at the /blog route.</p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio id cupiditate ex dolorum voluptates voluptas suscipit. Asperiores, in. Aperiam autem saepe molestiae ipsa veniam nihil ducimus, cum consequatur sit dolor?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quibusdam praesentium quae sequi eaque illo numquam ab hic repellendus cupiditate iste error doloremque officiis optio, explicabo vel perspiciatis impedit labore!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, incidunt eos eveniet deleniti, veritatis porro doloribus ipsam nam officia omnis sapiente quae. Exercitationem laboriosam iste nobis hic ullam, dicta perferendis?
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat obcaecati ex repellendus provident aperiam omnis quod cupiditate dolore culpa recusandae esse, suscipit officia quas nesciunt debitis? Iusto pariatur omnis deleniti?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, architecto. Corrupti itaque repellendus saepe quas, delectus atque enim. Sequi aperiam quidem veritatis repellendus, sit cum? Voluptates quos nobis mollitia maxime.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid repellendus vitae ducimus hic in quasi reiciendis ex magni ipsam, itaque obcaecati omnis dolore nisi accusantium magnam, vel, repudiandae saepe illum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, rem suscipit ipsa earum nesciunt odit tenetur reprehenderit, ipsum fugiat commodi fuga. Cupiditate nesciunt sint eius velit accusantium facilis dolorum similique.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae distinctio at quae soluta et eius illo. Ut iusto corporis vero aliquid sed odio voluptates iste eveniet, ipsum, doloribus ab.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis delectus saepe. Animi nisi exercitationem laboriosam dolore fugiat nulla dolores nostrum id natus, officiis minima eum repudiandae saepe assumenda quis!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eos illo reprehenderit? Eos, dolorum illum. Incidunt nam architecto sit consequatur nihil necessitatibus, voluptatem dolor? Officiis aut adipisci error mollitia iusto?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, facilis consequatur! Sunt consequatur doloribus iusto eum nihil, culpa tempora cumque vero magni? Sint omnis fugiat quaerat soluta vel non explicabo.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla sunt corporis reprehenderit repellat aspernatur. Natus, laboriosam? Doloribus quasi illo, veniam quaerat quisquam eius ducimus facilis nesciunt commodi aperiam praesentium!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sapiente nam voluptatem animi fuga vitae, voluptas recusandae cupiditate ipsam autem deleniti dignissimos. Iure eius quis, possimus qui suscipit impedit aliquam!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa adipisci deleniti, est explicabo consequatur laborum unde excepturi inventore repellendus necessitatibus facilis aspernatur omnis natus accusantium molestiae soluta magni corrupti illo?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum hic similique sed corporis fugiat, adipisci amet cupiditate vel nulla officia iste, eveniet provident inventore eligendi ipsa esse quo, laboriosam in?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium recusandae nemo magni perferendis quod, fuga tempore, et esse temporibus unde qui voluptate laborum. Perspiciatis cumque minima nobis quis fugiat ipsam.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio tenetur dolore molestiae perspiciatis commodi saepe et illum veritatis esse dolor, quod similique hic molestias voluptas temporibus assumenda iure ut id?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugiat tempore optio? Tempora, voluptates. Maxime quod debitis magni cum amet fugiat ab qui culpa aut enim, perferendis ipsam quidem corporis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nisi fugiat minima corporis! Non tempore similique odio deserunt corporis, rerum sequi autem magni illum, consequatur quidem dolores? Quo, dolores totam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi provident, quam qui praesentium porro distinctio, non numquam debitis magnam obcaecati veritatis in beatae odio saepe incidunt excepturi adipisci ea sapiente?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur a sapiente laborum veritatis quod suscipit officiis inventore distinctio labore! Esse ducimus, saepe velit in nihil itaque deleniti facere fuga odit?
     </section>
 </template>
