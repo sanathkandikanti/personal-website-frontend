e850f0ebbc4a3bebda37a4dbefe341c6f353eb3f
Sanath Kandikanti <sanathkandikanti@gmail.com>
2023-04-09 17:24:07 +0530

    Setup basic home page

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,8 +3,7 @@ import "vuetify/styles";
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
-    ssr: true,
-    
+    ssr: true
   })
 
   nuxtApp.vueApp.use(vuetify)
