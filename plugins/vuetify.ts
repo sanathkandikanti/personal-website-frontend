9f24d0554f5b4369384b954294388831deb48d24
Sanath Kandikanti <sanathkandikanti@gmail.com>
2023-11-26 19:30:56 +0530

    turn on ssr

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,7 +3,7 @@ import "vuetify/styles";
 import "assets/overrides.css"
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
-    ssr: false
+    ssr: true
   })
 
   nuxtApp.vueApp.use(vuetify)
