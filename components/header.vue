8c90151f24f3b39c9d027c58c58f71eea8c38814
Sanath Kandikanti <sanathkandikanti@gmail.com>
2023-11-26 16:53:55 +0530

    Fix padding in header

--- components/header.vue
+++ components/header.vue
@@ -1,50 +1,57 @@
 <template>
     <header class="header">
-      <v-container>
-        <v-row align="center">
-          <v-col cols="6">
-            <nav>
-              <ul class="menu-items">
-                <li><nuxt-link to="/" exact>About</nuxt-link></li>
-                <li><nuxt-link to="/notes">Notes</nuxt-link></li>
-                <li><nuxt-link to="/contact">Contact</nuxt-link></li>
-              </ul>
-            </nav>
-          </v-col>
-          <v-col cols="6" class="text-right">
-            <span class="header-text"><nuxt-link to="/">Sanath</nuxt-link></span>
-          </v-col>
-        </v-row>
-      </v-container>
+        <v-container>
+            <v-row align="center">
+                <v-col cols="6" class="no-padding">
+                    <nav>
+                        <ul class="menu-items">
+                            <li><nuxt-link to="/" exact>About</nuxt-link></li>
+                            <li><nuxt-link to="/notes">Notes</nuxt-link></li>
+                            <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+                        </ul>
+                    </nav>
+                </v-col>
+                <v-col cols="6" class="text-right no-padding">
+                    <span class="header-text"><nuxt-link to="/">Sanath</nuxt-link></span>
+                </v-col>
+            </v-row>
+        </v-container>
     </header>
-  </template>
+</template>
   
-  <style scoped>
-  .menu-items {
+<style scoped>
+
+/* Additional class to remove padding */
+.no-padding {
+  padding: 0 !important;
+}
+
+.menu-items {
     display: flex;
     list-style: none;
     padding: 0;
-  }
-  
-  .menu-items li {
+    padding-left: 0;
+}
+
+.menu-items li {
     margin-right: 20px;
-  }
-  
-  .menu-items li:last-child {
+}
+
+.menu-items li:last-child {
     margin-right: 0;
-  }
-  
-  nav ul li a {
+}
+
+nav ul li a {
     text-decoration: none;
     color: #333333;
     font-weight: 500;
     transition: color 0.3s, border-bottom 0.3s;
     font-size: 1.25rem;
     position: relative;
-  }
-  
-  /* Underline effect */
-  nav ul li a::after {
+}
+
+/* Underline effect */
+nav ul li a::after {
     content: '';
     display: block;
     position: absolute;
@@ -53,45 +60,45 @@
     height: 1px;
     background-color: transparent;
     transition: width 0.3s ease, background-color 0.3s ease;
-  }
-  
-  /* Animate underline on hover */
-  nav ul li a:hover::after {
+}
+
+/* Animate underline on hover */
+nav ul li a:hover::after {
     width: 100%;
     background-color: black;
-  }
-  
-  /* Animate underline on hover for selected link */
-  nav ul li .router-link-exact-active:hover::after {
+}
+
+/* Animate underline on hover for selected link */
+nav ul li .router-link-exact-active:hover::after {
     width: 100%;
     background-color: #1ad6ff;
-  }
-  
-  /* Active/Selected link without underline */
-  .router-link-exact-active::after {
+}
+
+/* Active/Selected link without underline */
+.router-link-exact-active::after {
     width: 0;
     background-color: transparent;
-  }
-  
-  /* Selected link text color */
-  .router-link-exact-active {
+}
+
+/* Selected link text color */
+.router-link-exact-active {
     color: #1ad6ff;
-  }
-  
-  .header-text {
+}
+
+.header-text {
     text-decoration: none;
     color: #333333;
     font-weight: bold;
     font-size: 2.5rem;
-  }
-  
-  .header-text a {
+}
+
+.header-text a {
     color: #333333;
     text-decoration: none;
-  }
-  
-  .text-right {
+}
+
+.text-right {
     text-align: right;
-  }
-  </style>
+}
+</style>
   
