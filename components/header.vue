d69fe3932e4da26bb2883d6c3f07be203cdcfaca
Sanath Kandikanti <sanathkandikanti@gmail.com>
2023-11-26 14:33:01 +0530

    Added styling for header buttons

--- components/header.vue
+++ components/header.vue
@@ -1,82 +1,117 @@
 <template>
     <header class="header">
-      <div class="header-content">
-        <nav>
-          <ul>
-            <li><nuxt-link to="/" exact>About</nuxt-link></li>
-            <li><nuxt-link to="/notes">Notes</nuxt-link></li>
-            <li><nuxt-link to="/contact">Contact</nuxt-link></li>
-          </ul>
-        </nav>
-        <span class="header-text"><nuxt-link to="/">Sanath</nuxt-link></span>
-      </div>
+        <div class="header-content">
+            <nav>
+                <ul>
+                    <li><nuxt-link to="/" exact>About</nuxt-link></li>
+                    <li><nuxt-link to="/notes">Notes</nuxt-link></li>
+                    <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+                </ul>
+            </nav>
+            <span class="header-text"><nuxt-link to="/">Sanath</nuxt-link></span>
+        </div>
     </header>
-  </template>
+</template>
   
-  <style scoped>
+<style scoped>
 .header {
-  background-color: #ffffff;
-  padding: 20px 0;
-  width: 100%;
-  margin: 0 auto;
+    background-color: #ffffff;
+    padding-top: 4rem;
+    padding-bottom: 4rem;
+    width: 100%;
+    margin: 0 auto;
 }
 
-  .header-content {
+.header-content {
     max-width: 1200px;
     margin: 0 auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 20px 0;
-  }
-
-  @media screen and (max-width: 768px) {
-  /* Adjust max-width for smaller screens */
-  .header-content {
-    max-width: 90%; 
-  }
 }
 
-  
-  nav ul {
+@media screen and (max-width: 768px) {
+
+    /* Adjust max-width for smaller screens */
+    .header-content {
+        max-width: 90%;
+    }
+}
+
+
+nav ul {
     list-style: none;
     display: flex;
     justify-content: flex-start;
     align-items: center;
     padding: 0;
     margin: 0;
-  }
-  
-  nav ul li {
+}
+
+nav ul li {
     margin-right: 20px;
-  }
-  
-  nav ul li:last-child {
+}
+
+nav ul li:last-child {
     margin-right: 0;
-  }
-  
-  nav ul li a {
-    text-decoration: none;
-    color: #333333;
-    font-weight: bold;
-    padding: 10px 15px;
-    transition: background-color 0.3s ease;
-  }
-  
-  .router-link-exact-active {
+}
+
+nav ul li a {
+  text-decoration: none;
+  color: #333333;
+  font-weight: 500;
+  padding: 10px 15px;
+  transition: color 0.3s, border-bottom 0.3s; /* Added transition for underline effect */
+  font-size: 1.25rem; /* Adjust font size */
+  position: relative; /* Create space for the underline */
+}
+
+/* Underline effect */
+nav ul li a::after {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: -2px;
+  width: 0;
+  height: 1px;
+  background-color: #000; /* Black underline for unselected elements */
+  transition: width 0.3s ease, background-color 0.3s ease; /* Added transition for underline color */
+}
+
+/* Animate underline on hover */
+nav ul li a:hover::after {
+  width: 100%;
+  background-color: black; /* Change underline color on hover */
+}
+
+/* Active/Selected link without underline */
+.router-link-exact-active::after {
+  width: 0; /* Hide underline after selection */
+  background-color: transparent; /* No underline for selected link */
+}
+
+/* Selected link text color */
+.router-link-exact-active {
+  color: #1ad6ff; /* Change text color for selected link */
+}
+
+
+
+.router-link-exact-active {
     color: #1ad6ff;
-  }
-  
-  .header-text {
+}
+
+.header-text {
     text-decoration: none;
     color: #333333;
     font-weight: bold;
-    font-size: 48px;
-  }
-  
-  .header-text a {
+    font-size: 2.5rem;
+}
+
+.header-text a {
     color: #333333;
     text-decoration: none;
-  }
-  </style>
+}
+</style>
   
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,46 @@
 <template>
     <div class="page-wrapper">
-        <Header />
-        <div class="content-wrapper">
-            <slot />
-        </div>
+      <Header />
+      <div class="content-wrapper">
+        <slot />
+      </div>
     </div>
-</template>
+  </template>
   
-<script>
-import Header from '~/components/header.vue';
-
-export default {
+  <script>
+  import Header from '~/components/header.vue';
+  
+  export default {
     components: {
-        Header,
+      Header,
     },
-};
-</script>
+  };
+  </script>
   
-<style>
-.page-wrapper {
+  <style>
+  .page-wrapper {
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 0 20px;
-}
-
-.content-wrapper {
+    padding: 0 20px; /* Add padding to the page wrapper */
+  }
+  
+  .content-wrapper {
     width: 100%;
     max-width: 1200px;
-    margin: 0 auto;
-}
+    margin: 0 auto; /* Center the content */
+  }
 
-@media screen and (min-width: 1200px) {
-    .page-wrapper {
-        padding: 0 400px;
-        /* Large screens: set padding to 400px */
-    }
+  /* Adjust padding for different screen sizes */
+  @media screen and (min-width: 1200px) {
+  .page-wrapper {
+    padding: 0 400px; /* Large screens: set padding to 400px */
+  }
 }
 
 @media screen and (max-width: 768px) {
-    .page-wrapper {
-        padding: 0 10px;
-        /* Small screens: set padding to 10px */
-    }
-}</style>
+  .page-wrapper {
+    padding: 0 10px; /* Small screens: set padding to 10px */
+  }
+}
+  </style>
